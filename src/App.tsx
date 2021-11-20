9381766716e3ad22461bda246a276bbffaac7078
Jose Gomes <josegomes.grego@outlook.com>
2021-11-20 18:58:47 -0300

    Add task

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./Types/Item";
 import { ListItem } from "./Components/ListItem/index";
+import { AddArea } from "./Components/AddArea";
 
 const App = () => {
     const [list, setList] = useState<Item[] >([
@@ -9,11 +10,22 @@ const App = () => {
         {id: 91, name: "Purchase cake", done: false},
     ]);
 
+    const handleAddTask = (taskName: string) => {
+        let newList = [...list];
+        newList.push({
+            id: (list.length + 1),
+            name: taskName,
+            done: false,
+        })
+        setList(newList);
+    }
+
     return (
         <Fragment>
             <C.Container>
                 <C.Area>
                     <C.Header>To do list</C.Header>
+                    <AddArea onAdd={handleAddTask} />
                     {list.map((item, index) => (
                         <ListItem key={index} item={item}/>
                     ))}
---
+++ src/Components/AddArea/index.tsx
@@ -0,0 +1,39 @@
+import { useState, KeyboardEvent } from "react";
+import * as C from "./styles";
+
+type Props = {
+    onAdd: (taskName: string) => void;
+}
+
+export const AddArea = ({ onAdd }: Props) => {
+    const [inputText, setInputText] = useState("");
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code === "Enter" && inputText !== "") {
+            onAdd(inputText);
+            setInputText("");
+        }
+    }
+
+    /*
+    const handleKeyUp = (e: KeyboardEvent) => {
+        ((e.code === "Enter" && inputText !== "") && (
+            onAdd(inputText)
+            setInputText("")
+        ));
+    }
+    */
+
+    return (
+        <C.Container>
+            <div className={"add"}>+</div>
+            <input
+                type={"text"}
+                placeholder={"Add new task - click enter to add"}
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+            />
+        </C.Container>
+    );
+}
