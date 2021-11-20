7c0dd3a2800b377b1dab084c0e2f972c615a6f29
Jose Gomes <josegomes.grego@outlook.com>
2021-11-20 18:02:59 -0300

    Styles 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./Types/Item";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, world!</p>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [list, setList] = useState<Item[] >([
+        {id: 90, name: "Purchase bread", done: false},
+        {id: 91, name: "Purchase cake", done: false},
+    ]);
+
+    return (
+        <Fragment>
+            <C.Container>
+                <C.Area>
+                    <C.Header>To do list</C.Header>
+                    {list.map((Item, index) => (
+                        <div key={index}>
+                            {Item.name}
+                            {Item.done}
+                        </div>
+                    ))}
+                </C.Area>
+            </C.Container>
+        </Fragment>
+    );
 }
 
 export default App;
