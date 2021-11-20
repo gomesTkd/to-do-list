17ae780e8ac4b1ad949b3198f914cf8a63648bce
Jose Gomes <josegomes.grego@outlook.com>
2021-11-20 18:25:18 -0300

    list item component and check

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./Types/Item";
+import { ListItem } from "./Components/ListItem/index";
 
 const App = () => {
     const [list, setList] = useState<Item[] >([
@@ -13,11 +14,8 @@ const App = () => {
             <C.Container>
                 <C.Area>
                     <C.Header>To do list</C.Header>
-                    {list.map((Item, index) => (
-                        <div key={index}>
-                            {Item.name}
-                            {Item.done}
-                        </div>
+                    {list.map((item, index) => (
+                        <ListItem key={index} item={item}/>
                     ))}
                 </C.Area>
             </C.Container>
